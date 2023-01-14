b08e8385ccdfa960d023bdc8acc1749061c61b1f
melumilic <melumilic@gmail.com>
2023-01-13 22:30:59 -0800

    Add option to roll D20

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,9 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
-import org.w3c.dom.Text
+
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
@@ -17,19 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.rollDiceButton)
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener { rollDice(6) }
+        val rollButton2: Button = findViewById(R.id.rollDiceButton1)
+        rollButton2.setOnClickListener { rollDice(20) }
     }
     /**
      * Roll the dice and update the screen with the result.
      */
-    private fun rollDice() {
+    private fun rollDice(numSides: Int) {
         // Create new Dice object with 6 sides and roll it
-        val dice = Dice(6)
+        val dice = Dice(numSides)
         val diceRoll = dice.roll()
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.diceResultText)
         resultTextView.text = diceRoll.toString()
     }
+
 }
 
 class Dice(private val numSides: Int) {
